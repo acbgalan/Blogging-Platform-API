2637c8060af48792686692f0399732ddfd3d3fb3
Carlos Blanco <acbgalan@gmail.com>
2025-07-17 16:04:16 +0200

    Database context, connection string, and service configuration for use with dependency injection

--- src/BloggingPlatform.Server/Program.cs
+++ src/BloggingPlatform.Server/Program.cs
@@ -1,8 +1,14 @@
+using BloggingPlatform.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
