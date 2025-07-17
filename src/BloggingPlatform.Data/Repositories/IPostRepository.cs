ce6fb2dc62b0914f7ac5604e8ffdd0e64072ea5e
Carlos Blanco <acbgalan@gmail.com>
2025-07-17 19:51:29 +0200

    Initial Post repository implementation. Additional methods may be added as needed

---
+++ src/BloggingPlatform.Data/Repositories/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using BloggingPlatform.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggingPlatform.Data.Repositories
+{
+    public interface IPostRepository : IRepositoryAsync<Post>
+    {
+    }
+}
