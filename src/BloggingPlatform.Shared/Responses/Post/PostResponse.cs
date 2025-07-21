3627e57c6ee9c10fcc9298690ffc591b8812abf6
Carlos Blanco <acbgalan@gmail.com>
2025-07-21 17:56:32 +0200

    Post DTOs

---
+++ src/BloggingPlatform.Shared/Responses/Post/PostResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggingPlatform.Shared.Responses.Post
+{
+    public class PostResponse
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required string Category { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
