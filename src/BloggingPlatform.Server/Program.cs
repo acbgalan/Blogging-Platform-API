1dc70afbb7ac2c70407aa044337733fa9f5c9c09
Carlos Blanco <acbgalan@gmail.com>
2025-07-21 18:02:40 +0200

    AutoMapper is added as a service, and the issue of circular references is resolved.

--- src/BloggingPlatform.Server/Program.cs
+++ src/BloggingPlatform.Server/Program.cs
@@ -1,15 +1,16 @@
 using BloggingPlatform.Data.Context;
 using BloggingPlatform.Data.Repositories;
+using BloggingPlatform.Server.Mapper;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationConnection")));
 builder.Services.AddScoped<IPostRepository, PostRepository>();
-
+builder.Services.AddAutoMapper(cfg => { cfg.LicenseKey = builder.Configuration["AutoMapper:LicenseKey"]; }, typeof(AutoMapperProfiles));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
