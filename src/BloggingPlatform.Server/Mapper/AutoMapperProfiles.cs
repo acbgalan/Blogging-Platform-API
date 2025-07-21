9fc937d574446b6b5e9e09ad7fad3167cf25a53d
Carlos Blanco <acbgalan@gmail.com>
2025-07-21 17:59:52 +0200

    Automapper profile that maps CreatePostRequest to Post class

---
+++ src/BloggingPlatform.Server/Mapper/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BloggingPlatform.Data.Entities;
+using BloggingPlatform.Shared.Requests.Post;
+using BloggingPlatform.Shared.Responses.Post;
+
+namespace BloggingPlatform.Server.Mapper
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<CreatePostRequest, Post>()
+                .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags.Select(tagName => new Tag { Name = tagName }).ToList()));
+        }
+    }
+}
