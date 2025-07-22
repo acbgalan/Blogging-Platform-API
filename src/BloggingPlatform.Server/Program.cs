82c7e7b9cf56abfe682d76b14922526e3e545fee
Carlos Blanco <acbgalan@gmail.com>
2025-07-22 16:10:48 +0200

    Configure swagger info

--- src/BloggingPlatform.Server/Program.cs
+++ src/BloggingPlatform.Server/Program.cs
@@ -14,7 +14,21 @@ builder.Services.AddAutoMapper(cfg => { cfg.LicenseKey = builder.Configuration["
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Blogging Platform API",
+        Version = "v1",
+        Description = "Backend Projects (roadmap.sh) - Blogging Platform API. Build a RESTful API for a personal blogging platform",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Carlos Blanco",
+            Email = "acbgalan@gmail.com",
+            Url = new Uri("https://www.carlosblanco.dev/")
+        }
+    });
+});
 
 var app = builder.Build();
 
